691bdee0342033bcc47b6cfdaf7993aa62a86284
Luis Martinez de Bartolome <lasote@gmail.com>
2017-07-19 17:00:31 +0200

    cleaner recipe

--- conanfile.py
+++ conanfile.py
@@ -1,55 +1,58 @@
+import os
+import shutil
+
+from conans import CMake, tools
 from conans import ConanFile
-import os, shutil
-from conans.tools import download, unzip, replace_in_file, check_md5
-from conans import CMake
 
 
 class Bzip2Conan(ConanFile):
     name = "bzip2"
     version = "1.0.6"
     branch = "master"
-    ZIP_FOLDER_NAME = "bzip2-%s" % version
     generators = "cmake"
-    settings =  "os", "compiler", "arch", "build_type"
+    settings = "os", "compiler", "arch", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = "shared=False", "fPIC=True"
     exports = ["CMakeLists.txt"]
-    url="http://github.com/lasote/conan-bzip2"
-    license="BSD-style license"
+    url = "http://github.com/lasote/conan-bzip2"
+    license = "BSD-style license"
+    description = "bzip2 is a freely available, patent free (see below), high-quality data " \
+                  "compressor. It typically compresses files to within 10% to 15% of the best" \
+                  " available techniques (the PPM family of statistical compressors), whilst being " \
+                  "around twice as fast at compression and six times faster at decompression."
+
+    @property
+    def zip_folder_name(self):
+        return "bzip2-%s" % self.version
 
     def config(self):
-        try: # Try catch can be removed when conan 0.8 is released
-            del self.settings.compiler.libcxx
-        except: 
-            pass        
+        del self.settings.compiler.libcxx
 
     def source(self):
         zip_name = "bzip2-%s.tar.gz" % self.version
-        download("http://www.bzip.org/%s/%s" % (self.version, zip_name), zip_name)
-        check_md5(zip_name, "00b516f4704d4a7cb50a1d97e6e8e15b")
-        unzip(zip_name)
-        
+        tools.download("http://www.bzip.org/%s/%s" % (self.version, zip_name), zip_name)
+        tools.check_md5(zip_name, "00b516f4704d4a7cb50a1d97e6e8e15b")
+        tools.unzip(zip_name)
         os.unlink(zip_name)
 
     def build(self):
-        shutil.move("CMakeLists.txt", "%s/CMakeLists.txt" % self.ZIP_FOLDER_NAME)
-        cmake = CMake(self.settings)
-
-        self.run("cd %s &&  mkdir _build" % self.ZIP_FOLDER_NAME)
-        cd_build = "cd %s/_build" % self.ZIP_FOLDER_NAME
-        shared = "-DBUILD_SHARED_LIBS=ON" if self.options.shared else ""
-        fpic = "-DFPIC=ON" if self.options.fPIC else ""
-        self.run('%s && cmake .. %s %s %s' % (cd_build, cmake.command_line, shared, fpic))
-        self.run("%s && cmake --build . %s" % (cd_build, cmake.build_config))
+        shutil.move("CMakeLists.txt", "%s/CMakeLists.txt" % self.zip_folder_name)
+        with tools.chdir(self.zip_folder_name):
+            tools.mkdir("_build")
+            with tools.chdir("_build"):
+                cmake = CMake(self)
+                if self.options.fPIC:
+                    cmake.definitions["FPIC"] = "ON"
+                cmake.configure(build_dir=".", source_dir="..")
+                cmake.build(build_dir=".")
 
     def package(self):
-        # Copying zlib.h, zutil.h, zconf.h
-        self.copy("*.h", "include", "%s" % (self.ZIP_FOLDER_NAME), keep_path=False)
-        self.copy("*bzip2", dst="bin", src=self.ZIP_FOLDER_NAME, keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", src=self.ZIP_FOLDER_NAME, keep_path=False)
-        self.copy(pattern="*.a", dst="lib", src="%s/_build" % self.ZIP_FOLDER_NAME, keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", src="%s/_build" % self.ZIP_FOLDER_NAME, keep_path=False)
-        self.copy(pattern="*.dll", dst="bin", src="%s/_build" % self.ZIP_FOLDER_NAME, keep_path=False)
+        self.copy("*.h", "include", "%s" % self.zip_folder_name, keep_path=False)
+        self.copy("*bzip2", dst="bin", src=self.zip_folder_name, keep_path=False)
+        self.copy(pattern="*.so*", dst="lib", src=self.zip_folder_name, keep_path=False)
+        self.copy(pattern="*.a", dst="lib", src="%s/_build" % self.zip_folder_name, keep_path=False)
+        self.copy(pattern="*.lib", dst="lib", src="%s/_build" % self.zip_folder_name, keep_path=False)
+        self.copy(pattern="*.dll", dst="bin", src="%s/_build" % self.zip_folder_name, keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ['bz2']
