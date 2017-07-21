3ec75025ee2ae6fff5f470999d90ba1943411292
Luis Martinez de Bartolome <lasote@gmail.com>
2017-07-21 09:55:50 +0200

    Removed requires in test package

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -2,22 +2,11 @@ from conans.model.conan_file import ConanFile
 from conans import CMake
 import os
 
-
-
-############### CONFIGURE THESE VALUES ##################
-default_user = "lasote"
-default_channel = "testing"
-#########################################################
-
-channel = os.getenv("CONAN_CHANNEL", default_channel)
-username = os.getenv("CONAN_USERNAME", default_user)
-
 class DefaultNameConan(ConanFile):
     name = "DefaultName"
     version = "0.1"
     settings = "os", "compiler", "arch", "build_type"
     generators = "cmake"
-    requires = "bzip2/1.0.6@%s/%s" % (username, channel)
 
     def build(self):
         cmake = CMake(self)
